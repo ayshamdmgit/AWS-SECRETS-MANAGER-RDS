217a5b86f676fdcd14202f47fe5e2d095872a1c8
ayshamdmgit <54041344+ayshamdmgit@users.noreply.github.com>
2020-04-05 18:26:11 -0400

    Create rds_setup.tf

---
+++ terraform/rds_setup.tf
@@ -0,0 +1,17 @@
+resource "aws_db_subnet_group" "subnet_group" {
+  name       = "main"
+  subnet_ids = ["Subnet_id_1", "Subnet_id_2"]
+}
+
+resource "aws_db_instance" "test_db" {
+  db_subnet_group_name = aws_db_subnet_group.subnet_group.name   
+  allocated_storage    = 64
+  engine               = "postgres"
+  identifier.          = "rds-instance"
+  instance_class       = "db.t2.micro"
+  name                 = "postgres"
+  username             = "admin_user"
+  password             = "foobarbaz"
+  iam_database_authentication_enabled = true
+  vpc_security_group_ids = ["Sgs_1", "Sgs_2"....]
+}
